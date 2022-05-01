577ea1fb97436c3fe7fb0db79a764b79f4659aa6
Phyf3 <phade075@gmail.com>
2022-05-01 22:36:55 +0100

    overview-mobile

--- src/pages/overview.jsx
+++ src/pages/overview.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import pointer from '../images/target.svg'
+import '../styles/overview.css'
+import shower from '../images/Shower.png'
+
 
 const Overview = () => {
   return (
-    <div>Overview</div>
+    <div className='overview'>
+        <header className='overview_header' >
+            <div className='search_btn-container' >
+                <button>Search for places </button>
+            </div>
+
+            <div>
+                <img className='pointer' src={pointer} alt="" />
+            </div>
+        </header>
+
+        <main>
+            <section>
+                <img src={shower} alt="" />
+            </section> 
+
+            <section>
+                <div className='degree' >
+                    <h1> <span>15</span>°C </h1>
+                </div>
+                <div className='weather-type'>
+                    <h2>Shower</h2>
+                </div>
+                <div className='day'>
+                    <span>Today</span>
+                    <span>Fri,5 Jun</span>
+                </div>
+                <div className="location">
+                    Helsinki
+                </div>
+            </section>
+        </main>
+    </div>
   )
 }
 
